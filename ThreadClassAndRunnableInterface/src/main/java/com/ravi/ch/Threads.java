289a93bc18e34e5647646efeb322ce0abad5946f
Ravi Chauhan <ravich@cisco.com>
2025-06-28 17:42:11 +0530

    Creating Threads by extending Thread & implementing runnable

--- ThreadClassAndRunnableInterface/src/main/java/com/ravi/ch/Threads.java
+++ ThreadClassAndRunnableInterface/src/main/java/com/ravi/ch/Threads.java
@@ -1,10 +1,40 @@
 package com.ravi.ch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Threads {
+
+    private static final Logger logger = LoggerFactory.getLogger(Threads.class);
+
+    private int findPower(int n, int x){
+        if (n == 0) return x >= 1?1:0;
+        if (x == 0) return 1;
+        if (x == 1) return n;
+        if ((x&1) == 1) {
+            return n * findPower(x-1,n);
+        } else {
+            return findPower(n*n,x/2);
+        }
+    }
+
     public static void main(String[] args) {
-        CustomThreadClass thread1 = new CustomThreadClass();
-        CustomThreadClass thread2 = new CustomThreadClass();
+
+        CustomThreadClass thread1 = new CustomThreadClass(5);
+        CustomThreadClass thread2 = new CustomThreadClass(10);
+
         thread1.start();
         thread2.start();
+
+        logger.info("{} creates thread via extending Thread",thread1);
+
+        CustomThreadInterface runnable = new CustomThreadInterface();
+        Thread thread3 = new Thread(runnable);
+
+        thread3.start();
+
+        logger.info("{} creates new thread by CustomThreadInterface and we pass the custom runnable"
+                ,thread2);
+
     }
 }
