b7e4dede93e25b8c871e6938551dbf24c7442628
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2025-07-07 23:03:47 +0530

    CONCURRENCY-02: Fizz Buzz problem (#3)
    * FizzBuzz concurrency problem
    
    * Read me Update
    
    ---------
    
    Co-authored-by: Ravi Chauhan <ravich@cisco.com>

---
+++ FizzBuzz/src/main/java/com/ravi/ch/FizzBuzz.java
@@ -0,0 +1,64 @@
+package com.ravi.ch;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class FizzBuzz {
+    private int num;
+    private int count;
+    private Semaphore fizzLock;
+    private Semaphore buzzLock;
+    private Semaphore fizzBuzzLock;
+    private Semaphore numLock;
+    private Semaphore mutex;
+
+    FizzBuzz(int num){
+        this.num = num;
+        this.count = 1;
+        this.fizzLock = new Semaphore(0);
+        this.buzzLock = new Semaphore(0);
+        this.fizzBuzzLock = new Semaphore(0);
+        this.numLock = new Semaphore(1);
+        this.mutex = new Semaphore(0);
+    }
+
+    public void number() throws InterruptedException{
+        while(count< num && numLock.tryAcquire(1, TimeUnit.SECONDS)){
+            System.out.println(count);
+            count++;
+            if (count%3 == 0 && count%5 == 0){
+                fizzBuzzLock.release();
+            } else if (count%3 == 0){
+                fizzLock.release();
+            } else if (count%5 == 0){
+                buzzLock.release();
+            } else {
+                numLock.release();
+            }
+        }
+    }
+
+    public void fizzBuzz() throws InterruptedException{
+        while(count< num && fizzBuzzLock.tryAcquire(1,TimeUnit.SECONDS)){
+            System.out.println("FizzBuzz");
+            count++;
+            numLock.release();
+        }
+    }
+
+    public void buzz() throws InterruptedException{
+        while(count< num && buzzLock.tryAcquire(1, TimeUnit.SECONDS)){
+            System.out.println("Buzz");
+            count++;
+            numLock.release();
+        }
+    }
+
+    public void fizz() throws InterruptedException{
+        while(count< num && fizzLock.tryAcquire(1, TimeUnit.SECONDS)){
+            System.out.println("Fizz");
+            count++;
+            numLock.release();
+        }
+    }
+}
